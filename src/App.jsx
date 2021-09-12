4d7dd7f61a13d112a1db85eb5831f4a2245e6358
douglasdl <douglas_san@hotmail.com>
2021-09-12 09:11:29 -0300

    Create Counter component to test its states

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './styles/global.scss';
 import { RepositoryList } from './components/RepositoryList';
+import { Counter } from './components/Counter';
 
 export function App() {
     //throw new Error('Errrrroooouuuu!!!!!')
     return (
-        <RepositoryList />
+        <>
+            <RepositoryList />
+
+            <Counter />
+        </>
+
     );
 }
